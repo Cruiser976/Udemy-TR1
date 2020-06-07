9dc0d047f29b6fdb728983ce4b0d4a3b1f14a704
Ralph Ellinger <R.Ellinger@live.de>
2020-06-07 18:14:43 +0200

    Methode für Addition erstellt

--- Program.cs
+++ Program.cs
@@ -10,18 +10,25 @@ namespace Udemy_TR1
     {
         static void Main(string[] args)
         {
+            //wandelt Text in Double
             double Zahl1, Zahl2, ErgebnisAddition;
             Console.Write("Gebe bitte die 1. Zahl ein: "); Zahl1 = Convert.ToDouble(Console.ReadLine());
             Console.Write("Gebe bitte die 2. Zahl ein: "); Zahl2 = Convert.ToDouble(Console.ReadLine());
 
-            //Adidtion
-            Console.WriteLine("Addition (+): Das Ergebnis lautet: " +(ErgebnisAddition=Zahl1+Zahl2) );
-            Console.WriteLine("\n\n(Enter drücken um Programm zu beenden)");
+            //Addition zweier Werte
+            Console.WriteLine("Addition (+): Das Ergebnis lautet: " + (ErgebnisAddition = Addiere(Zahl1,Zahl2)));
             WarteAufBenutzerEingabe();
+        }
+
+        static double Addiere(double Zahl1intern, double Zahl2intern)
+        {
+            double ErgebnisAdditionIntern = Zahl1intern + Zahl2intern;
+            return (ErgebnisAdditionIntern);
         }
 
         static void WarteAufBenutzerEingabe()
         {
+            Console.WriteLine("\n\n(Enter drücken um Programm zu beenden)");
             Console.ReadLine();
         }
     }
