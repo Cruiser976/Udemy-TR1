0b367eeb382f53296dbaa5f62ca59cfee66c0c11
Ralph Ellinger <R.Ellinger@live.de>
2020-06-07 08:07:05 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy_TR1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Zahl1, Zahl2, ErgebnisAddition;
+            Console.Write("Gebe bitte die 1. Zahl ein: "); Zahl1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Gebe bitte die 2. Zahl ein: "); Zahl2 = Convert.ToDouble(Console.ReadLine());
+
+            //Adidtion
+            Console.WriteLine("Addition (+): Das Ergebnis lautet: " +(ErgebnisAddition=Zahl1+Zahl2) );
+
+        }
+    }
+}
